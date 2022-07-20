e701ee044d8ff9abc0460ec619d5a4fafd4cd319
ja153903 <jaimeabbariao@gmail.com>
2022-07-19 22:48:29 -0400

    feat: adding a problem I solved

---
+++ src/leetcode/daily/__tests__/leetcode-792.test.ts
@@ -0,0 +1,10 @@
+import { numMatchingSubseq } from '../leetcode-792';
+
+describe('792. Number of Matching Subsequences', () => {
+  it('should pass the sample case', () => {
+    const s = 'abcde';
+    const words = ['a', 'bb', 'acd', 'ace'];
+
+    expect(numMatchingSubseq(s, words)).toBe(3);
+  });
+});
