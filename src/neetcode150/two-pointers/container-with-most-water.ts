5ea2eb7ccec6a57a3546d67bbd661868208ec147
ja153903 <jaimeabbariao@gmail.com>
2022-07-29 21:17:00 -0400

    feat: updating with another problem

--- src/neetcode150/two-pointers/container-with-most-water.ts
+++ src/neetcode150/two-pointers/container-with-most-water.ts
@@ -1,3 +1,21 @@
-function maxArea(height: number[]): number {}
+function maxArea(height: number[]): number {
+  let area = 0;
+
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    const currentArea = Math.min(height[left], height[right]) * (right - left);
+    area = Math.max(area, currentArea);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return area;
+}
 
 export { maxArea };
