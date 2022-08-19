36216d6e312cd3919d51126b7080550d081162be
ja153903 <jaimeabbariao@gmail.com>
2022-08-19 00:47:45 -0400

    feat: solving all the trie problems

---
+++ src/neetcode150/tries/design-add-and-search-words-data-structure.ts
@@ -0,0 +1,66 @@
+class TrieNode {
+  isWord: boolean;
+  children: Map<string, TrieNode>;
+
+  constructor() {
+    this.isWord = false;
+    this.children = new Map();
+  }
+
+  addWord(word: string) {
+    let current: TrieNode = this;
+
+    for (const ch of word) {
+      if (!current.children.has(ch)) {
+        current.children.set(ch, new TrieNode());
+      }
+
+      current = current.children.get(ch)!;
+    }
+
+    current.isWord = true;
+  }
+}
+
+class WordDictionary {
+  private root: TrieNode;
+
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  addWord(word: string): void {
+    this.root.addWord(word);
+  }
+
+  search(word: string): boolean {
+    return this.dfs(word, this.root, 0);
+  }
+
+  dfs(word: string, current: TrieNode = this.root, index: number = 0): boolean {
+    if (word.length === index) {
+      return current.isWord;
+    }
+
+    const char = word[index];
+
+    if (char === '.') {
+      for (const value of current.children.values()) {
+        if (this.dfs(word, value, index + 1)) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    // otherwise we should check if the current character is in the map
+    if (!current.children.has(char)) {
+      return false;
+    }
+
+    return this.dfs(word, current.children.get(char)!, index + 1);
+  }
+}
+
+export { WordDictionary };
--- src/neetcode150/tries/implement-trie.ts
+++ src/neetcode150/tries/implement-trie.ts
@@ -1,9 +1,53 @@
 class Trie {
-  constructor() {}
+  isWord: boolean;
+  children: Map<string, Trie>;
 
-  insert(word: string): void {}
+  constructor() {
+    this.isWord = false;
+    this.children = new Map();
+  }
 
-  search(word: string): boolean {}
+  insert(word: string): void {
+    let current: Trie = this;
 
-  startsWith(prefix: string): boolean {}
+    for (const ch of word) {
+      if (!current.children.has(ch)) {
+        current.children.set(ch, new Trie());
+      }
+
+      current = current.children.get(ch)!;
+    }
+
+    current.isWord = true;
+  }
+
+  search(word: string): boolean {
+    let current: Trie = this;
+
+    for (const ch of word) {
+      if (!current.children.has(ch)) {
+        return false;
+      }
+
+      current = current.children.get(ch)!;
+    }
+
+    return current.isWord;
+  }
+
+  startsWith(prefix: string): boolean {
+    let current: Trie = this;
+
+    for (const ch of prefix) {
+      if (!current.children.has(ch)) {
+        return false;
+      }
+
+      current = current.children.get(ch)!;
+    }
+
+    return true;
+  }
 }
+
+export { Trie };
