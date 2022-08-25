a9c660aa0b38088b69784d1450edf68adefa3d50
ja153903 <jaimeabbariao@gmail.com>
2022-08-25 01:43:38 -0400

    feat: adding some more problems

---
+++ src/leetcode/top-interview-questions/__tests__/roman-to-integer.test.ts
@@ -0,0 +1,8 @@
+import { romanToInt } from '../roman-to-integer';
+
+describe('Roman To Int', () => {
+  it('should pass a simple case', () => {
+    const s = 'LVIII';
+    expect(romanToInt(s)).toBe(58);
+  });
+});
