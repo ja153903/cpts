cd3d85fa81e89e338cfb57c1bfffdfc92698ecfc
ja153903 <jaimeabbariao@gmail.com>
2022-09-06 02:23:35 -0400

    feat: adding all changes

---
+++ src/leetcode/companies/microsoft/__tests__/longest-happy-string.test.ts
@@ -0,0 +1,8 @@
+import { longestDiverseString } from '../longest-happy-string';
+
+describe('Longest Happy String', () => {
+  it('should pass some test', () => {
+    longestDiverseString(1, 1, 3);
+    longestDiverseString(1, 1, 7);
+  });
+});
---
+++ src/leetcode/companies/microsoft/sign-of-the-product-of-the-array.ts
@@ -0,0 +1,15 @@
+function arraySign(nums: number[]): number {
+  let product = 1;
+
+  for (const num of nums) {
+    if (num === 0) {
+      return 0;
+    }
+
+    product *= num;
+  }
+
+  return product > 0 ? 1 : -1;
+}
+
+export { arraySign };
