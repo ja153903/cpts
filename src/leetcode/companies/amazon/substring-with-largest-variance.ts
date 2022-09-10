01da58d3ec8a89521161e1c7493988e2515f7232
ja153903 <jaimeabbariao@gmail.com>
2022-09-10 18:39:28 -0400

    feat: updating

---
+++ src/leetcode/companies/amazon/substring-with-largest-variance.ts
@@ -0,0 +1,44 @@
+/**
+ * The variance of a string is defined as the largest difference between the number of occurrences of any 2 characters present in the string.
+ * Note the two characters may or may not be the same.
+ *
+ * Given a string s consisting of lowercase English letters only, return the largest variance possible among all substrings of s.
+ *
+ * A substring is a contiguous sequence of characters within a string.
+ */
+function largestVariance(s: string): number {
+  // a hint I got here was to apply some variation of Kadane's algorithm
+  // how would we apply kadane here?
+  // we can keep increasing the size of the substring we're working with
+  return -1;
+}
+
+// This problem in the worst case will take O(n^2 lg n)
+// a pure optimization may take O(n^2)
+function largestVarianceBruteForce(s: string): number {
+  let variance = 0;
+
+  // go through every possible substring
+  for (let i = 0; i < s.length; i++) {
+    const map = new Map<string, number>();
+
+    for (let j = i; j < s.length; j++) {
+      map.set(s[j], (map.get(s[j]) ?? 0) + 1);
+      variance = Math.max(variance, getVariance([...map.values()]));
+    }
+  }
+
+  return variance;
+}
+
+function getVariance(values: number[]): number {
+  if (values.length < 2) {
+    return 0;
+  }
+
+  values.sort((a, b) => a - b);
+
+  return values[values.length - 1] - values[0];
+}
+
+export { largestVariance, largestVarianceBruteForce };
--- src/leetcode/companies/google/step-by-step-directions-from-binary-tree-node-to-another.ts
+++ src/leetcode/companies/google/step-by-step-directions-from-binary-tree-node-to-another.ts
@@ -1,12 +1,51 @@
 import { TreeNode } from '~/leetcode/data-structures/tree-node';
 
-// TODO: Should attempt this problem
 function getDirections(
   root: TreeNode | null,
   startValue: number,
   destValue: number
 ): string {
-  return '';
+  const pathToStart = generatePath(root, startValue);
+  const pathToDest = generatePath(root, destValue);
+
+  // now that we have both paths, we should prune the paths
+  // while they are equal
+  let i = 0;
+  while (
+    i < pathToStart.length &&
+    i < pathToDest.length &&
+    pathToStart[i] === pathToDest[i]
+  ) {
+    // note that they both start from the root, so we prune it like this
+    i++;
+  }
+
+  let fromStart = '';
+
+  if (i < pathToStart.length) {
+    fromStart = 'U'.repeat(pathToStart.length - i);
+  }
+
+  return fromStart + pathToDest.substring(i);
+}
+
+function generatePath(
+  root: TreeNode | null,
+  value: number,
+  acc: string = ''
+): string {
+  if (root == null) {
+    return '';
+  }
+
+  if (root.val === value) {
+    return acc;
+  }
+
+  return (
+    generatePath(root.left, value, acc + 'L') +
+    generatePath(root.right, value, acc + 'R')
+  );
 }
 
 export { getDirections };
