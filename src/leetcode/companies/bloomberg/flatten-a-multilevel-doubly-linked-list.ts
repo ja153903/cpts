10b9feccb9ad7cd317f542a73c7a0fbde8a5a8a3
ja153903 <jaimeabbariao@gmail.com>
2022-09-13 20:15:53 -0400

    feat: finishing flatten multilevel doubly linked list

--- src/leetcode/companies/bloomberg/flatten-a-multilevel-doubly-linked-list.ts
+++ src/leetcode/companies/bloomberg/flatten-a-multilevel-doubly-linked-list.ts
@@ -11,9 +11,50 @@ class Node {
   }
 }
 
-// TODO: Finish this problem
 function flatten(head: Node | null): Node | null {
-  return null;
+  // the reason we care about this being a doubly linked list is because
+  // we want to be able to connect previous and next nodes when we can
+  // we only really do that though when there exists a child
+  // however, we should somehow also keep track of nodes we've already visited
+  let current: Node | null = head;
+
+  while (current !== null) {
+    if (!current.child) {
+      current = current.next;
+      continue;
+    }
+    // in the case that there's a child, we have to make sure that
+    // we connect all the possible nodes before we move on
+    // if any of the children have a child themselves, we don't have to worry
+    // about that until later
+
+    let temp = current.child;
+
+    // when temp.next is null, we'll want to fill that in
+    // so we exit then
+    while (temp?.next) {
+      temp = temp.next;
+    }
+
+    // connect end of children to current.next
+    temp.next = current.next;
+
+    // if current.next is not null, then make sure
+    // that node's previous node is the the tail
+    // of the children nodes
+    if (current?.next) {
+      current.next.prev = temp;
+    }
+
+    // current.next should be the current child
+    current.next = current.child;
+    // previous node the child should then be current
+    current.child.prev = current;
+    // make sure that child is now null
+    current.child = null;
+  }
+
+  return head;
 }
 
 export { flatten };
