9e71c44369381ef76a27b9a34d97d01680044bb0
ja153903 <jaimeabbariao@gmail.com>
2022-09-14 23:24:23 -0400

    feat: adding some more solutions

--- src/leetcode/companies/snapchat/best-meeting-point.ts
+++ src/leetcode/companies/snapchat/best-meeting-point.ts
@@ -1,5 +1,7 @@
 // TODO: Finish this problem later
 function minTotalDistance(grid: number[][]): number {
+  // given we have a binary matrix where we're trying to minimize the total distance
+  // between all possible friends to a certain meeting place
   return -1;
 }
 
