ded1a5bcf7398f8656041059598b1964bf479c26
ja153903 <jaimeabbariao@gmail.com>
2022-09-18 11:27:57 -0400

    feat: adding middle of linked list problem

---
+++ src/grind75/week2/middle-of-linked-list.ts
@@ -0,0 +1,19 @@
+import ListNode from '~/utility/lists';
+
+function middleNode(head: ListNode | null): ListNode | null {
+  if (!head) {
+    return null;
+  }
+
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  while (slow && fast?.next) {
+    slow = slow.next;
+    fast = fast.next?.next;
+  }
+
+  return slow;
+}
+
+export { middleNode };
