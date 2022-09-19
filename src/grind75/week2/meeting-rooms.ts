51a8b74e66bdc5678f803ea060a4e9b8b7c7a0a8
ja153903 <jaimeabbariao@gmail.com>
2022-09-19 14:34:24 -0400

    feat: adding meeting-rooms

---
+++ src/grind75/week2/meeting-rooms.ts
@@ -0,0 +1,20 @@
+// we should sort the intervals by
+// the beginning of the interval and then by the end
+// suppose that we have something like: [[0,30],[5,10],[15,20]]
+// if anything overlaps, then we return false
+function canAttendMeetings(intervals: number[][]): boolean {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  for (let i = 1; i < intervals.length; i++) {
+    const prev = intervals[i - 1];
+    const curr = intervals[i];
+
+    if (curr[0] < prev[1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export { canAttendMeetings };
