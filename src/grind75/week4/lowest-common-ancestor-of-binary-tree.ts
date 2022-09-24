e5a8b7d86262526daeb6a35575600bd4fd17a81c
ja153903 <jaimeabbariao@gmail.com>
2022-09-24 09:22:55 -0400

    feat: adding a new solution

--- src/grind75/week4/lowest-common-ancestor-of-binary-tree.ts
+++ src/grind75/week4/lowest-common-ancestor-of-binary-tree.ts
@@ -1,12 +1,30 @@
 import TreeNode from '~/utility/trees';
 
-// TODO: Do this problem
 function lowestCommonAncestor(
   root: TreeNode | null,
   p: TreeNode | null,
   q: TreeNode | null
 ): TreeNode | null {
-  return null;
+  // base case: if root is null, then return null
+  if (root === null) {
+    return null;
+  }
+
+  // if the root is either p or q, then we break early
+  // and return the root
+  if (root === p || root === q) {
+    return root;
+  }
+
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+
+  // if both exist, then we know that root is the closest
+  if (left && right) {
+    return root;
+  }
+
+  return left ? left : right;
 }
 
 export { lowestCommonAncestor };
