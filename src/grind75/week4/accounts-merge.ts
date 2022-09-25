74178b978a9113bc669c090bf53ce8f0ec363e6a
ja153903 <jaimeabbariao@gmail.com>
2022-09-25 12:31:02 -0400

    feat: updating with new problems

---
+++ src/grind75/week4/accounts-merge.ts
@@ -0,0 +1,39 @@
+// This problem is a UnionFind problem
+// because of how we're trying to connect each email to its
+import { UnionFind } from '~/utility/graphs';
+
+// corresponding account
+function accountsMerge(accounts: string[][]): string[][] {
+  const uf = new UnionFind(accounts.length);
+
+  const ownership = new Map<string, number>();
+
+  for (let i = 0; i < accounts.length; i++) {
+    const [_, ...emails] = accounts[i];
+    for (const email of emails) {
+      if (ownership.has(email)) {
+        uf.union(i, ownership.get(email)!);
+      }
+
+      ownership.set(email, i);
+    }
+  }
+
+  const result = new Map<number, Array<string>>();
+  for (const [email, owner] of ownership.entries()) {
+    const parent = uf.find(owner);
+
+    if (!result.has(parent)) {
+      result.set(parent, []);
+    }
+
+    result.get(parent)?.push(email);
+  }
+
+  return [...result.entries()].map(([owner, emails]) => [
+    accounts[owner][0],
+    ...emails.sort(),
+  ]);
+}
+
+export { accountsMerge };
---
+++ src/grind75/week4/time-based-key-value-store.ts
@@ -0,0 +1,48 @@
+type Entry = { value: string; timestamp: number };
+
+class TimeMap {
+  private map: Map<string, Entry[]>;
+
+  constructor() {
+    this.map = new Map();
+  }
+
+  set(key: string, value: string, timestamp: number): void {
+    if (!this.map.has(key)) {
+      this.map.set(key, []);
+    }
+
+    this.map.get(key)?.push({ value, timestamp });
+  }
+
+  get(key: string, timestamp: number): string {
+    const entries = this.map.get(key) ?? null;
+    if (!entries) {
+      return '';
+    }
+
+    let left = 0;
+    let right = entries.length;
+
+    // with our binary search what we want to achieve
+    // if the highest such number that is less than
+    // or equal to the current timestamp
+    while (left < right) {
+      const mid = left + Math.floor((right - left) / 2);
+
+      // if the current timestamp is less than what we want
+      // then we increase left
+      // otherwise, we assign mid to right (this denotes that right)
+      // should contain our answer
+      if (entries[mid].timestamp <= timestamp) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+
+    return right === 0 ? '' : entries[right - 1].value;
+  }
+}
+
+export { TimeMap };
