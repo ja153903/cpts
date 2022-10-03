e54081e36b24a3d40c98bc9f3412bcfd9509a931
ja153903 <jaimeabbariao@gmail.com>
2022-10-02 22:57:20 -0400

    feat: adding some stuff

--- src/grind75/week6/minimum-height-trees.ts
+++ src/grind75/week6/minimum-height-trees.ts
@@ -3,19 +3,7 @@ function findMinHeightTrees(n: number, edges: number[][]): number[] {
     return new Array<number>(n).fill(0).map((_value, index) => index);
   }
 
-  const graph = new Map<number, Set<number>>();
-  for (const [u, v] of edges) {
-    if (!graph.has(u)) {
-      graph.set(u, new Set());
-    }
-
-    if (!graph.has(v)) {
-      graph.set(v, new Set());
-    }
-
-    graph.get(u)?.add(v);
-    graph.get(v)?.add(u);
-  }
+  const graph = createUndirectedGraph(edges);
 
   const result: number[] = new Array<number>(n).fill(Number.POSITIVE_INFINITY);
 
@@ -64,4 +52,67 @@ function depth(
   return result;
 }
 
-export { findMinHeightTrees };
+// LEET: https://leetcode.com/problems/minimum-height-trees/solutions/76055/share-some-thoughts/?orderBy=most_votes
+// LEET: https://leetcode.com/problems/minimum-height-trees/solutions/900035/minimum-height-trees/
+function topologicalSortSolution(n: number, edges: number[][]): number[] {
+  if (n === 1) {
+    return [0];
+  }
+
+  // Create an undirected graph using a map
+  const graph = createUndirectedGraph(edges);
+  let leaves: number[] = [];
+
+  // keep track of all the nodes where they are only connected to 1 other node
+  for (let i = 0; i < n; i++) {
+    const children = graph.get(i);
+    if (children?.size === 1) {
+      leaves.push(i);
+    }
+  }
+
+  while (n > 2) {
+    // subtract the number of nodes we need to consider from our solution
+    n -= leaves.length;
+    const newLeaves: number[] = [];
+    for (const node of leaves) {
+      // for each leaf node, we can survey the node its connected to
+      const child = graph.get(node)?.values()?.next()?.value;
+      if (child != null) {
+        // if such a node exists, we should get rid of its reference to
+        // the leaf node.
+        graph.get(child)?.delete(node);
+        // if that node now is only connected to 1 other node,
+        // then we know it's a leaf node so we push it into a new queue
+        if (graph.get(child)?.size === 1) {
+          newLeaves.push(child);
+        }
+      }
+    }
+
+    // we update our leaves to hold the new set of leaves
+    leaves = newLeaves;
+  }
+
+  return leaves;
+}
+
+function createUndirectedGraph(edges: number[][]): Map<number, Set<number>> {
+  const graph = new Map<number, Set<number>>();
+  for (const [u, v] of edges) {
+    if (!graph.has(u)) {
+      graph.set(u, new Set());
+    }
+
+    if (!graph.has(v)) {
+      graph.set(v, new Set());
+    }
+
+    graph.get(u)?.add(v);
+    graph.get(v)?.add(u);
+  }
+
+  return graph;
+}
+
+export { findMinHeightTrees, topologicalSortSolution };
---
+++ todo.md
@@ -0,0 +1,4 @@
+# Things to do
+
+- [ ] Create utility for testing Tree problems
+- [ ] Create utility for testing List problems
