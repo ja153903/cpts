adc0de0eee81d1809e7f1acb463d5b5dc232ab1b
ja153903 <jaimeabbariao@gmail.com>
2022-10-12 23:32:22 -0400

    feat: updating with more stuff

---
+++ src/neetcodeAll/arrays-and-hashing/longest-consecutive-sequence.ts
@@ -0,0 +1,21 @@
+function longestConsecutive(nums: number[]): number {
+  const uniqueNums = new Set<number>(nums);
+  let maxSequence = 0;
+
+  for (const num of nums) {
+    if (!uniqueNums.has(num - 1)) {
+      let current = num;
+      let sequence = 0;
+      while (uniqueNums.has(current)) {
+        sequence++;
+        current++;
+      }
+
+      maxSequence = Math.max(maxSequence, sequence);
+    }
+  }
+
+  return maxSequence;
+}
+
+export { longestConsecutive };
---
+++ src/neetcodeAll/arrays-and-hashing/sort-colors.ts
@@ -0,0 +1,22 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {
+  const counter = new Array<number>(3).fill(0);
+
+  for (const num of nums) {
+    counter[num]++;
+  }
+
+  let j = 0;
+
+  for (let i = 0; i < counter.length; i++) {
+    let count = counter[i];
+
+    while (count > 0) {
+      nums[j] = i;
+      j++;
+      count--;
+    }
+  }
+}
