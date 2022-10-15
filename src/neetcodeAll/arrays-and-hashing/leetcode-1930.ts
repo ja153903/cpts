394c2a51a5284a1ee06724865e7615291cf129b2
ja153903 <jaimeabbariao@gmail.com>
2022-10-15 12:01:47 -0400

    feat: updating

--- src/neetcodeAll/arrays-and-hashing/leetcode-1930.ts
+++ src/neetcodeAll/arrays-and-hashing/leetcode-1930.ts
@@ -1,6 +1,25 @@
-// TODO: Solve in the morning
 function countPalindromicSubsequence(s: string): number {
-  return 0;
+  let result = 0;
+  const indicesByChar = new Map<string, number[]>();
+
+  for (let i = 0; i < s.length; i++) {
+    if (!indicesByChar.has(s[i])) {
+      indicesByChar.set(s[i], []);
+    }
+
+    indicesByChar.get(s[i])?.push(i);
+  }
+
+  for (const [_char, indices] of indicesByChar.entries()) {
+    const first = indices[0];
+    const last = indices[indices.length - 1];
+
+    if (first < last) {
+      result += new Set<string>(s.substring(first + 1, last).split('')).size;
+    }
+  }
+
+  return result;
 }
 
 export { countPalindromicSubsequence };
