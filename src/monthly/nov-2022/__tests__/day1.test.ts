995f6f8024434be2fb3a6cbd28146598c5f545d8
ja153903 <jaimeabbariao@gmail.com>
2022-11-01 01:03:30 -0400

    feat: solving daily

--- src/monthly/nov-2022/__tests__/day1.test.ts
+++ src/monthly/nov-2022/__tests__/day1.test.ts
@@ -12,4 +12,21 @@ describe('findBall()', () => {
 
     expect(findBall(grid)).toEqual([1, -1, -1, -1, -1]);
   });
+
+  it('should pass another sample case', () => {
+    const grid = [[-1]];
+
+    expect(findBall(grid)).toEqual([-1]);
+  });
+
+  it('should pass the final sample case', () => {
+    const grid = [
+      [1, 1, 1, 1, 1, 1],
+      [-1, -1, -1, -1, -1, -1],
+      [1, 1, 1, 1, 1, 1],
+      [-1, -1, -1, -1, -1, -1],
+    ];
+
+    expect(findBall(grid)).toEqual([0, 1, 2, 3, 4, -1]);
+  });
 });
--- src/monthly/nov-2022/day1.ts
+++ src/monthly/nov-2022/day1.ts
@@ -1,5 +1,43 @@
 function findBall(grid: number[][]): number[] {
-  return [];
+  const result = new Array<number>(grid[0].length).fill(-1);
+
+  for (let col = 0; col < grid[0].length; col++) {
+    result[col] = dfs(grid, col);
+  }
+
+  return result;
+}
+
+function dfs(grid: number[][], col: number): number {
+  for (let row = 0; row < grid.length; row++) {
+    const current = grid[row][col];
+
+    let nextCol = current === 1 ? col + 1 : col - 1;
+    // check if nextCol is inBounds
+    if (nextCol < 0 || nextCol >= grid[0].length) {
+      return -1;
+    }
+
+    // if boundary item tries to go out of bounds
+    if (
+      (current === 1 && col === grid[0].length - 1) ||
+      (current === -1 && col === 0)
+    ) {
+      return -1;
+    }
+
+    // check if the next value will create a basin
+    if (
+      (current === 1 && grid[row][col + 1] === -1) ||
+      (current === -1 && grid[row][col - 1] === 1)
+    ) {
+      return -1;
+    }
+
+    col = nextCol;
+  }
+
+  return col;
 }
 
 export { findBall };
